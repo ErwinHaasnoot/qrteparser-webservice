de9e42f1ca4ec57ad03b9c2bc6c5ede703659055
Erwin Haasnoot <e.haasnoot.3@umail.leidenuniv.nl>
2015-04-18 11:54:32 +0000

    Added skeleton for new Python parser

---
+++ qrte_parser_python/__init__.py
@@ -0,0 +1,1 @@
+from parser.py import QRTEParser
---
+++ qrte_parser_python/parser.py
@@ -0,0 +1,13 @@
+"""
+    Main file for the QRTE Python Parser.
+    
+"""
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    QRTEParser.parse('static/in/
